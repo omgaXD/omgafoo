8725c46d553250fd164c3061f350ef8169eca1f9
omgaXD <sladekop@gmail.com>
2026-09-01 20:23:31 +0500

    simplify simple group tokens to just two - it's enough

--- web/src/ts/group/simple.ts
+++ web/src/ts/group/simple.ts
@@ -5,21 +5,17 @@ import { FiniteTransformationMapper } from "./parse";
 
 type Model = [number, number, number, number];
 
-export class SimpleModelDefinition implements ModelDefinition<Model, 4> {
+export class SimpleModelDefinition implements ModelDefinition<Model, 2> {
 	baseModel(): Model {
 		return [0, 1, 2, 3];
 	}
-	applyTransformation(model: Model, transformation: (0 | 1 | 2 | 3)[]): Model {
+	applyTransformation(model: Model, transformation: (0 | 1)[]): Model {
 		return transformation.reduce((prev, t) => {
 			switch (t) {
 				case 0:
 					return [prev[0], prev[3], prev[2], prev[1]];
 				case 1:
-					return [prev[2], prev[1], prev[0], prev[3]];
-				case 2:
-					return [prev[1], prev[0], prev[2], prev[3]];
-				case 3:
-					return [prev[0], prev[1], prev[3], prev[2]];
+					return [prev[2], prev[0], prev[3], prev[1]];
 				default:
 					throw new Error(`Invalid transformation: ${transformation}`);
 			}
@@ -42,9 +38,9 @@ export class SimpleModelRepr implements ModelRepr<Model> {
 	}
 }
 
-export class SimpleRLUDMapper implements FiniteTransformationMapper<4> {
-	getValidTokens(): FixedArray<4, string> {
-		return ['r', 'l', 'u', 'd'];
+export class SimpleRLUDMapper implements FiniteTransformationMapper<2> {
+	getValidTokens(): FixedArray<2, string> {
+		return ['r', 'f'];
 	}
 	tokenSeparator(): " " | "" | "," {
 		return ''
